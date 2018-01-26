6f82b23e5310d78e360a44b03974dc4a86aa7395
dogancankilment <dogancankilment@gmail.com>
2018-01-26 09:38:06 +0300

    choices added for a new file, cause maybe extend

---
+++ redditanalyzer/choices.py
@@ -0,0 +1,8 @@
+STATUS_CHOICES = (
+    ('1', 'All Fields'),
+    ('2', 'Entry Title'),
+    ('3', 'Entry Author'),
+    ('4', 'Entry Author Url'),
+    ('5', 'Entry Updated Date'),
+    ('6', 'Entry Url'),
+)
