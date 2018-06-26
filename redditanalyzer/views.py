e78f586cd6a5cb9564d043f723c52948b7d464d2
dogancankilment <dogancankilment@gmail.com>
2018-06-26 20:52:29 +0300

    analysis function returned to dynamic shape, login required decorator added, it means the user has to be logged in for the query process, process saved to the database with user tag, and user can be reach old queries

--- redditanalyzer/views.py
+++ redditanalyzer/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from forms import ChoiceForm
+from authentication.models import Queries
+from django.contrib.auth.decorators import login_required
 import feedparser
 
 
-def home(request):
+@login_required()
+def analysis(request):
     form = ChoiceForm(request.POST or None)
 
     if request.POST:
-        query = feedparser.parse('https://www.reddit.com/r/utopianio/.rss')
+        query_url = request.POST["query_url"]
+        query = feedparser.parse(query_url + str('.rss'))
         entry_count = len(query['entries'])
         all_entries = query['entries']
         choice = request.POST["example-select"]
+
+        if request.user:
+            new_query = Queries()
+            new_query.url = query_url
+            new_query.which_user = request.user
+
+            new_query.save()
 
         c = {"request": request,
              "all_entries": all_entries,
@@ -28,3 +39,20 @@ def home(request):
     c.update(csrf(request))
 
     return render_to_response("forms/make-choice.html", c)
+
+
+def home(request):
+    c = {"request": request}
+    c.update(csrf(request))
+    return render_to_response("home/index.html", c)
+
+
+def show_old_queries(request):
+    all_queries = Queries.objects.filter(which_user=request.user)
+
+    c = {"request": request,
+         "all_queries": all_queries}
+
+    c.update(csrf(request))
+
+    return render_to_response("auth/queries.html", c)
