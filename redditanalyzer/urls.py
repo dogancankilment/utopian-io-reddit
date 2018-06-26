91365150d342f3c1b7e3a7288e1fae55a9ca2099
dogancankilment <dogancankilment@gmail.com>
2018-06-26 20:39:21 +0300

    authentication urls added, new url for analysis page, new url for showing old queries linked with user

--- redditanalyzer/urls.py
+++ redditanalyzer/urls.py
@@ -8,4 +8,24 @@ urlpatterns = patterns('',
                        url(r'^$',
                            'redditanalyzer.views.home',
                            name='home'),
+
+                       url(r'^redditanalysis$',
+                           'redditanalyzer.views.analysis',
+                           name='analysis'),
+
+                       url(r'^my-queries$',
+                           'redditanalyzer.views.show_old_queries',
+                           name='queries'),
+
+                       url(r'^login',
+                           'authentication.views.login',
+                           name='login'),
+
+                       url(r'^register',
+                           'authentication.views.register',
+                           name='register'),
+
+                       url(r'^logout$',
+                           'authentication.views.logout',
+                           name='logout'),
                        )
